217b8a64b5e654ba731e44e0349a1b6b16777d69
nikosaja99 <projectniko153@gmail.com>
2026-06-13 10:13:56 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,37 @@
-const CACHE_NAME = "time-skip-v1";
+const CACHE_NAME = "time-skip-v2";
 
 const FILES = [
-  "./",
-  "./index.html",
-  "./script.js",
-  "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  "/project-1/",
+  "/project-1/index.html",
+  "/project-1/script.js",
+  "/project-1/manifest.json",
+  "/project-1/icons/icon-192.png",
+  "/project-1/icons/icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
+
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES))
   );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then((res) => res || fetch(event.request))
   );
 });
