b1e567ea6f285fcf0d78527c554dffc383d93332
nikosaja99 <projectniko153@gmail.com>
2026-06-13 09:31:58 +0700

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,26 @@
 const CACHE_NAME = "time-skip-v1";
 
 const FILES = [
-    "./",
-    "./index.html",
-    "./script.js",
-    "./manifest.json"
+  "./",
+  "./index.html",
+  "./script.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(FILES);
-        })
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES);
+    })
+  );
 });
 
 self.addEventListener("fetch", (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
